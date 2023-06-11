e9a7ee5f8ec3cb451235a55c2b29bf7eb8542b64
David McNulty <david.mcnulty@gmail.com>
2023-06-11 18:33:22 -0400

    fix retry logic after Popen() refactor. Interpret Blender stdout and avoid info overload.

--- mander.py
+++ mander.py
@@ -4,8 +4,9 @@ import subprocess
 import argparse
 import time
 import logging
+import re
 
-logging.basicConfig(format='MANDER:%(levelname)s:%(message)s', level=logging.DEBUG)
+logging.basicConfig(format='MANDER:%(levelname)s:%(message)s', level=logging.INFO)
 
 
 RENDER_OUTPUT_BASE_DIR = 'C:\\blender_files\\renders'
@@ -56,30 +57,28 @@ def run(blender_cmd: BlenderCmd,
     else:
         frames_rendered = []
 
-    while exit_code not in [0, 1] \
-            and blender_cmd.end_frame not in frames_rendered \
-            and num_retries < max_retries:
-        try:
-            logging.debug(f"Blender Command: {' '.join(blender_cmd.command_line)}")
-            proc = subprocess.Popen(blender_cmd.command_line, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    while exit_code not in [0, 1] and blender_cmd.end_frame not in frames_rendered and num_retries < max_retries:
+        writer = BlenderLineInterpreter()
+        logging.info(f"Blender Command: {' '.join(blender_cmd.command_line)}")
+        proc = subprocess.Popen(blender_cmd.command_line, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        exit_code = proc.poll()
+        while True:
+            stdout = proc.stdout.readline()
+            if stdout == b'' and exit_code is not None:
+                break
+            if stdout:
+                writer.summarize(stdout.decode().strip())
             exit_code = proc.poll()
-            while True:
-                stdout = proc.stdout.readline()
-                if stdout == b'' and exit_code is not None:
-                    break
-                if stdout:
-                    print(stdout.decode().strip())
-                exit_code = proc.poll()
 
-            frames_rendered = get_frame_numbers_in_dir(blender_cmd.frame_output_path)
-            report_success(proc, blender_cmd)
+        frames_rendered = get_frame_numbers_in_dir(blender_cmd.frame_output_path)
+        report_success(proc, blender_cmd)
 
-        except subprocess.CalledProcessError as e:
+        if exit_code != 0:
             num_retries += 1
             frames_rendered = get_frame_numbers_in_dir(blender_cmd.frame_output_path)
             last_frame = max(frames_rendered) if frames_rendered else 0
-            logging.error(f'returncode: {e.returncode}. Failed at frame: {last_frame}. '
-                          f'retrying {max_retries - num_retries} more times...:\n {e}')
+            logging.error(f'returncode: {proc.returncode}. Failed at frame: {last_frame}. '
+                          f'retrying {max_retries - num_retries} more times...')
             if num_retries < max_retries:
                 blender_cmd.start_frame = last_frame + 1
 
@@ -87,7 +86,7 @@ def run(blender_cmd: BlenderCmd,
 def report_success(completed_proc, blender_cmd: BlenderCmd):
     # print(completed_proc.stdout.decode())
     logging.info(f"Frames Rendered:{len(get_frame_numbers_in_dir(blender_cmd.frame_output_path))}")
-    logging.info("Manager completed with returncode: " + str(completed_proc.returncode))
+    logging.debug("Manager completed with returncode: " + str(completed_proc.returncode))
 
 
 def get_scene_frames(project_path) -> (int, int):
@@ -130,6 +129,41 @@ def new_frame_output_dir(project_file_path: str):
     name = project_file_path.split("\\")[-1].split(".blend")[0]
     project_render_dir = '_'.join([name, SCRIPT_START_TIMESTAMP])
     return os.path.join(RENDER_OUTPUT_BASE_DIR, project_render_dir)
+
+
+class BlenderLineInterpreter:
+    saved_expr = re.compile(r"Saved: '(.+)'")
+    time_expr = re.compile(r"Time: (\d+:?\d+:\d+\.\d+) \(Saving: (\d+:?\d+:\d+\.\d+)\)")
+
+    def __init__(self, status_message_frequency: int = 30):
+        self.status_msg_freq = status_message_frequency  # max freq of frame-render status messages. still alive msg.
+        self.last_report_time = time.time()
+
+        self.saved_filename = None
+        self.render_time = None
+
+    def summarize(self, line: str):
+        logging.debug(line)
+
+        if line.startswith('Saved:'):
+            try:
+                self.saved_filename = self.saved_expr.match(line).group(1)
+            except (AttributeError, IndexError):
+                self.saved_filename = f"Unrecognized Saved File Path: {line}"  # probably never hit this
+
+        elif line.startswith("Fra:") and (current_time := time.time()) - self.last_report_time > self.status_msg_freq:
+            print(line)
+            self.last_report_time = current_time
+
+        elif line.startswith('Time:'):
+            try:
+                self.render_time = self.time_expr.match(line).group(1)
+            except (AttributeError, IndexError):
+                self.render_time = f"Unrecognized Render Time: {line}"  # probably never hit this
+
+            # "Time: ..." Indicates final log message for a frame. Print cached stuff.
+            print(f"{self.saved_filename}: {self.render_time}")
+            self.last_report_time = time.time()
 
 
 if __name__ == '__main__':
